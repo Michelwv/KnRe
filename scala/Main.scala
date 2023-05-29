b3569d95f49ce4679642974e4a927fe6a068d527
Michelwv <82516079+Michelwv@users.noreply.github.com>
2023-05-29 11:33:28 +0200

    Create makeHittingTree

--- scala/Main.scala
+++ scala/Main.scala
@@ -1,20 +1,23 @@
 import Diagnosis._
 import Conflicts.tpf
+import gapt.expr.formula.Formula
 import gapt.expr.stringInterpolationForExpressions
+import gapt.expr.formula.fol.FOLTerm
 
 
 object Main extends App {
-  println("Running diagnostics on problem 1 with an empty list of broken components..")
   // Try adding different elements of the set of components to this list for the problems in Diagnosis
   val test = List(
     fot"a1",
-    fot"a2"
+    fot"a2",
+    fot"o1"
   )
+
 
   val all = test.toSet.subsets.map(_.toList).toList
 
-  for (component <- all){
-    val result = tpf(problem1, component)
+  for (component <- all) {
+    val result = tpf(problem3, component)
 
     result match {
       case Some(res) =>
@@ -27,13 +30,38 @@ object Main extends App {
         println()
     }
   }
+
+  sealed abstract class Tree
+
+  case object Leaf extends Tree
+
+  case class Node(elem: Option[Set[FOLTerm]],
+                  children: List[Tree])
+    extends Tree
+
+  def makeHittingTree(problem: () => (List[Formula], List[FOLTerm], List[Formula]), HS: List[FOLTerm] = List()): Tree = {
+    val result = tpf(problem, HS)
+    result match {
+      case Some(conflictSet) =>
+        val children: List[Tree] = conflictSet.toList.flatMap { element =>
+          val newHS = HS :+ element
+          Some(makeHittingTree(problem, newHS))
+        }
+        Node(result, children)
+
+      case None => Leaf
+    }
+  }
+
+  print(makeHittingTree(problem3, Nil))
+}
   //val hs = List(fot"a1")
   // Try different problems and observe what the outcomes are, also try putting different combinations of components in hs
   //val Some(result) = tpf(problem1, hs)
   //val None = tpf(problem1, hs)
   //println("Conflict sets: ")
   //result.foreach(println)
-}
+
   /*sealed abstract class Tree
 
   case object Leaf extends Tree
